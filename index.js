6379e3257a409ba7c6c8b26d909ca9521e162570
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-17 16:49:20 -0300

    Apresentando o Jest

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-import  assert from 'node:assert/strict';
+// import assert from 'node:assert/strict';
 
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verifiqueSe = (valor) => {
-    const assercoes = {
-        ehExatamenteIgualA(esperado) {
-            assert.strictEqual(valor, esperado);
-        }
-    };
-    return assercoes;
+export {
+    somaHorasExtras,
+    calculaDescontos
 };
 
-const teste = (titulo, funcaoDeTeste) => {
-    try {
-        funcaoDeTeste();
-        console.log(`${titulo} passou!`);
-    } catch (error) {
-        console.error(`${titulo} não passou!!!`);
-    }
-};
 
-teste('somaHorasExtras', () => {
-    const esperado = 2500;
-    const retornado = somaHorasExtras(2000, 500);
+// const verifiqueSe = (valor) => {
+//     const assercoes = {
+//         ehExatamenteIgualA(esperado) {
+//             assert.strictEqual(valor, esperado);
+//         }
+//     };
+//     return assercoes;
+// };
 
-    verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
+// const teste = (titulo, funcaoDeTeste) => {
+//     try {
+//         funcaoDeTeste();
+//         console.log(`${titulo} passou!`);
+//     } catch (error) {
+//         console.error(`${titulo} não passou!!!`);
+//     }
+// };
 
-teste('calculaDesconto', () => {
-    const esperado = 2300;
-    const retornado = calculaDescontos(2500, 200);
+// teste('somaHorasExtras', () => {
+//     const esperado = 2500;
+//     const retornado = somaHorasExtras(2000, 500);
 
-    verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
+//     verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+// });
+
+// teste('calculaDesconto', () => {
+//     const esperado = 2300;
+//     const retornado = calculaDescontos(2500, 200);
+
+//     verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+// });
