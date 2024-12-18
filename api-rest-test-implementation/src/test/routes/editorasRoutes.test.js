3ce468d99068cedb4a8f09cd0fa1dcf31e3b15c6
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-18 13:01:32 -0300

    Supertest

--- api-rest-test-implementation/src/test/routes/editorasRoutes.test.js
+++ api-rest-test-implementation/src/test/routes/editorasRoutes.test.js
@@ -1,8 +1,9 @@
+import request from 'supertest';
+import { describe } from '@jest/globals';
 import app from '../../app.js';
 
 // hooks -> ganchos
 let server;
-
 beforeEach(() => {
   const port = 3000;
   server = app.listen(port);
@@ -10,4 +11,47 @@ beforeEach(() => {
 
 afterEach(() => {
   server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let respostId;
+describe('POST em /editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const reposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 's@s.com',
+      })
+      .expect(201);
+    respostId = reposta.body.content.id;
+  });
+});
+
+describe('GET em /editoras/id', () => {
+  it('Deve retornar o recurso selecionado', async () => {
+    await request(app)
+      .get(`/editoras/${respostId}`)
+      .expect(200);
+  });
+});
+
+describe('DELETE em /editoras/id', () => {
+  it('Deletar o recusos adicionado!', async () => {
+    await request(app)
+      .delete(`/editoras/${respostId}`)
+      .expect(200);
+  });
 });
